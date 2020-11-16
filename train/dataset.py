d8ee015fdf578959e42104107cef1e2bfc39a74f
plutasnyy <kamil.plucinski97@gmail.com>
2020-11-16 22:22:57 +0100

    try cropped faces

--- train/dataset.py
+++ train/dataset.py
@@ -15,6 +15,7 @@ class FaceDataModule(pl.LightningDataModule):
         self.batch_size = batch_size
 
         self.preprocess_train = A.Compose([
+            A.RandomCrop(150,150),
             A.Resize(224, 224),
             A.Rotate(limit=20, p=0.25),
             A.OpticalDistortion(p=0.25),
@@ -23,6 +24,7 @@ class FaceDataModule(pl.LightningDataModule):
             ToTensorV2()
         ])
         self.preprocess_valid = A.Compose([
+            A.CenterCrop(150,150),
             A.Resize(224, 224),
             A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ToTensorV2()
