ea5eb42e6f7e676f22966d86bcdf11d3a441391f
plutasnyy <kamil.plucinski97@gmail.com>
2020-11-16 12:52:53 +0100

    add logging

--- train/model.py
+++ train/model.py
@@ -1,15 +1,11 @@
-from operator import itemgetter
-
 import pytorch_lightning as pl
 from torch import nn
 from torch.nn.functional import mse_loss
 from torch.optim import Adam
 from torchvision import models
-import numpy as np
 
 
 class MobileNetLightingModel(pl.LightningModule):
-    """https://pytorch.org/hub/pytorch_vision_mobilenet_v2/"""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -21,23 +17,16 @@ class MobileNetLightingModel(pl.LightningModule):
 
     def training_step(self, batch, batch_nb):
         x, y = batch
-        y_hat = self.forward(x).float()
+        y_hat = self.forward(x).squeeze()
         loss = mse_loss(y_hat, y.float())
-        return {'loss': loss, 'train_mse': loss.item()}
-
-    def training_epoch_end(self, outputs):
-        avg_train_mse = np.mean(list(map(itemgetter('train_mse'), outputs)))
-        self.logger.log_metrics({'train_mse': avg_train_mse}, step=self.current_epoch)
+        self.log('train_mse', loss.item(), logger=True, on_step=False, on_epoch=True)
+        return {'loss': loss}
 
     def validation_step(self, batch, batch_nb):
         x, y = batch
-        y_hat = self.forward(x).float()
+        y_hat = self.forward(x).squeeze()
         loss = mse_loss(y_hat, y.float())
-        return {'valid_mse': loss.item()}
-
-    def validation_epoch_end(self, outputs):
-        avg_valid_mse = np.mean(list(map(itemgetter('valid_mse'), outputs)))
-        self.logger.log_metrics({'valid_mse': avg_valid_mse}, step=self.current_epoch)
+        self.log('val_mse', loss.item(), prog_bar=True, logger=True, on_step=False, on_epoch=True)
 
     def configure_optimizers(self):
         optimizer = Adam(self.parameters())
--- train/train.py
+++ train/train.py
@@ -1,21 +1,28 @@
+from pathlib import Path
+
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
 from pytorch_lightning.loggers import CometLogger
 
 from dataset import FaceDataModule
 from model import MobileNetLightingModel
 
+LOGGING_PARAMS = {'comet_ml_logging': True}
+LEARNING_PARAMS = {'epochs': 2}
+ALL_PARAMS = {**LOGGING_PARAMS, **LEARNING_PARAMS}
+
 data_module = FaceDataModule('data/imdb-wiki/wiki_crop_aligned')
 model = MobileNetLightingModel()
-comet_logger = CometLogger(api_key="2ma9DWG8F7ul8RsBQTcXy3pCz", project_name="pbp", workspace="plutasnyy")
-model_checkpoint = ModelCheckpoint(filepath='checkpoints/{epoch:02d}-{val_loss:.2f}',
-                                   save_weights_only=True,
-                                   save_top_k=3,
-                                   monitor='val_loss',
-                                   period=1)
 
-trainer = Trainer(logger=comet_logger, max_epochs=2, gpus=1, checkpoints=model_checkpoint)
-for k in model_checkpoint.best_k_models.keys():
-    model_name = 'checkpoints/' + k.split('/')[-1]
-    comet_logger.experiment.log_artifact(k, model_name)
-comet_logger.experiment.set_property('best_model_score', model_checkpoint.best_model_score.tolist())
+comet_logger = CometLogger(api_key="2ma9DWG8F7ul8RsBQTcXy3pCz", project_name="pbp", workspace="plutasnyy")
+comet_logger.log_hyperparams(ALL_PARAMS)
+model_checkpoint = ModelCheckpoint(filepath='checkpoints/{epoch:02d}-{val_mse:.2f}', save_weights_only=True,
+                                   save_top_k=3, monitor='val_mse', period=1)
+lr_logger = LearningRateMonitor(logging_interval='epoch')
+
+trainer = Trainer(logger=comet_logger, max_epochs=LEARNING_PARAMS['epochs'], callbacks=[model_checkpoint, lr_logger])
+trainer.fit(model, datamodule=data_module)
+
+for absolute_path in model_checkpoint.best_k_models.keys():
+    comet_logger.experiment.log_model(Path(absolute_path).name, absolute_path)
+comet_logger.log_metrics({'best_model_score': model_checkpoint.best_model_score.tolist()})
